e3d1517d5247d9592d7060b53338916c199aad54
Artem illushchenko <artemillushchenko30@gmail.com>
2026-03-09 14:54:34 +0200

    ДЗ 7

---
+++ components/NoteList/NoteList.tsx
@@ -0,0 +1,41 @@
+import css from "./NoteList.module.css";
+import type { Note } from "@/types/note";
+import { deleteNote } from "@/lib/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import Link from "next/link";
+
+export interface NoteListProps {
+  notes: Note[];
+}
+
+export default function NoteList({ notes }: NoteListProps) {
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: (id: string) => deleteNote(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
+  });
+
+  return (
+    <ul className={css.list}>
+      {notes.map((note) => (
+        <li key={note.id ?? note.title} className={css.listItem}>
+          <h2 className={css.title}>{note.title}</h2>
+          <p className={css.content}>{note.content}</p>
+          <div className={css.footer}>
+            <span className={css.tag}>{note.tag}</span>
+              <Link href={note.id ? `/notes/${note.id}` : "/notes/error"} className={css.viewLink}>View details</Link>
+            <button
+              className={css.button}
+              onClick={() => note.id && mutate(note.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </li>
+      ))}
+      </ul>
+  );
+}
